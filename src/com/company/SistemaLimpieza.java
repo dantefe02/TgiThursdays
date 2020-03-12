dbf7348bf1200de5f87f30d7f701c504342533c0
dante <dantefe02@gmail.com>
2020-03-12 20:57:23 -0300

    first commit

--- src/com/company/SistemaLimpieza.java
+++ src/com/company/SistemaLimpieza.java
@@ -1,14 +1,4 @@
 package com.company;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class SistemaLimpieza {
-    private List<Empleado> ListaDeEmpleados = new ArrayList<>();
-
-    public SistemaLimpieza(List<Empleado> listaDeEmpleados) {
-        ListaDeEmpleados = listaDeEmpleados;
-    }
-    
 }
