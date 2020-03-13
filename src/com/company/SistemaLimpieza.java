75eb7e9fe35161828a77a60c77491679ba24acc2
dante <dantefe02@gmail.com>
2020-03-12 21:02:09 -0300

    git ignore

--- src/com/company/SistemaLimpieza.java
+++ src/com/company/SistemaLimpieza.java
@@ -1,4 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaLimpieza {
+    private List<empleado> empleadoList = new ArrayList<>();
+
+    public SistemaLimpieza(List<empleado> empleadoList) {
+        this.empleadoList = empleadoList;
+    }
 }
--- src/com/company/empleado.java
+++ src/com/company/empleado.java
@@ -1,4 +1,27 @@
 package com.company;
 
 public class empleado {
+    private String nombre;
+    private Double Sueldo;
+
+    public empleado(String nombre, Double sueldo) {
+        this.nombre = nombre;
+        Sueldo = sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getSueldo() {
+        return Sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        Sueldo = sueldo;
+    }
 }
